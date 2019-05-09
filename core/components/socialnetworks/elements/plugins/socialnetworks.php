49abc5d258a1c74598f89f27c5faed1fb741af89
Aleksandr Huz <Superboshnik@yandex.ru>
2019-05-09 13:33:31 +0300

    Version 1.0.2

--- core/components/socialnetworks/elements/plugins/socialnetworks.php
+++ core/components/socialnetworks/elements/plugins/socialnetworks.php
@@ -3,7 +3,7 @@
 if ($modx->event->name == 'OnManagerPageInit') {
     $fontawesome_js = $modx->getOption('socialnetworks_fontawesome_js');
     if(!empty($fontawesome_js)) {
-        $fontawesome = '<script src="'.$fontawesome_js.'" integrity="sha384-4oV5EgaV02iISL2ban6c/RmotsABqE4yZxZLcYMAdG7FAPsyHYAPpywE9PJo+Khy" crossorigin="anonymous" defer></script>';
+        $fontawesome = '<script src="'.$fontawesome_js.'" defer></script>';
         $modx->regClientStartupScript($fontawesome);    
     }
 }
