4034c0acf82d3975d439b084a4df672ee93babbf
Aleksandr Huz <Superboshnik@yandex.ru>
2020-04-26 12:53:04 +0300

    Fontawesome font update to version 5.13.0

--- _build/elements/settings.php
+++ _build/elements/settings.php
@@ -4,16 +4,16 @@ return [
     'services' => [
         'xtype' => 'textfield',
         'value' => '[["behance","Behance"], ["dribbble", "Dribbble"], ["facebook-f","Facebook"], ["github","Github"],["google-plus-g","Google Plus"],["instagram","Instagram"],["linkedin","LinkedIn"],["odnoklassniki","Odnoklassniki"],["pinterest","Pinterest"],["skype","Skype"],["slack","Slack"],["telegram","Telegram"],["twitter","Twitter"],["viber","Viber"],["vimeo","Vimeo"],["vk","Vkontakte"],["whatsapp","WhatsApp"],["youtube","Youtube"]]',
-        'area' => 'default',
+        'area' => 'socialnetworks_default',
     ],
     'fontawesome_css' => [
         'xtype' => 'textfield',
-        'value' => '//use.fontawesome.com/releases/v5.8.1/css/all.css',
-        'area' => 'fontawesome',
+        'value' => '//use.fontawesome.com/releases/v5.13.0/css/all.css',
+        'area' => 'socialnetworks_fontawesome',
     ],
     'fontawesome_js' => [
         'xtype' => 'textfield',
-        'value' => '//use.fontawesome.com/releases/v5.8.1/js/all.js',
-        'area' => 'fontawesome',
+        'value' => '//use.fontawesome.com/releases/v5.13.0/js/all.js',
+        'area' => 'socialnetworks_fontawesome',
     ]
 ];
